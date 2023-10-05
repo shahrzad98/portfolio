0c224e48bd8ec72bf2f5396b6a0e5ecd4d8e7f60
shahrzad98 <shahrzad.gheidi@gmail.com>
2023-10-05 11:02:13 +0330

    change profile pic

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,59 @@
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {
-  About,
-  Contact,
-  Experience,
-  Hero,
-  Navbar,
-  Tech,
-  Projects,
+    About,
+    Contact,
+    Experience,
+    Hero,
+    Navbar,
+    Tech,
+    Projects,
 } from './components';
 import {styles} from "./styles.js";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="relative z-0">
-        <div>
-          <Navbar />
-          <Hero />
-        </div>
+    return (
+        <BrowserRouter>
+            <div className="relative z-0">
+                <div>
+                    <Navbar/>
+                    <Hero/>
+                </div>
 
-        <div className="bg-about bg-cover bg-center bg-no-repeat">
-          <About />
-        </div>
+                <div className="bg-about bg-cover bg-center bg-no-repeat">
+                    <About/>
+                </div>
 
-        <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
-          <Tech />
-        </div>
+                <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
+                    <Tech/>
+                </div>
 
-        <Projects />
+                <Projects/>
 
-        <div
-          className="bg-experience bg-cover bg-center bg-no-repeat 
+                <div
+                    className="bg-experience bg-cover bg-center bg-no-repeat
             rounded-tl-[150px] rounded-br-[150px]">
-          <div
-            className="bg-experienceLight bg-cover bg-center 
+                    <div
+                        className="bg-experienceLight bg-cover bg-center
             bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
-            <Experience />
-          </div>
-        </div>
-        <div className="relative z-0">
-          <Contact />
-          <div
-              className="flex flex-row w-[100%] my-10 justify-center  px-12">
-            <p className={styles.sectionSubText}>
-              Contact Number: &nbsp;
-            </p>
-            <div className='cursor-pointer' onClick={()=>
-                window.open("tel:900300400")}>
-              <p className={styles.sectionSubText}>00989127766194 </p>
+                        <Experience/>
+                    </div>
+                </div>
+                <div className="relative z-0">
+                    <Contact/>
+                    <div
+                        className="flex flex-row w-[100%] my-10 justify-center  px-12">
+                        <p className={styles.sectionSubText}>
+                            Contact Number: &nbsp;
+                        </p>
+                        <div className='cursor-pointer' onClick={() =>
+                            window.open("tel:900300400")}>
+                            <p className={styles.sectionSubText}>00989127766194 </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+        </BrowserRouter>
+    );
 };
 
 export default App;
